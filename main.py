8202b61ee05a94afb57c2b34289ba004f11653f4
ck <github@carrollkong.com>
2019-06-13 13:54:01 +0000

    added more skeleton code for AWSCloudBuddy and split up AWSCloudbuddy and Cloudbuddy

--- main.py
+++ main.py
@@ -17,8 +17,18 @@ import boto3
 #AWS_SESSION_TOKEN
 #The session key for your AWS account. This is only needed when you are using temporary credentials. The AWS_SECURITY_TOKEN environment variable can also be used, but is only supported for backwards compatibility purposes. AWS_SESSION_TOKEN is supported by multiple AWS SDKs besides python.
 
+# this will inherit aws cloud buddy OR we will use a function pointer
 class CloudBuddy:
+    def __init__(self):
+        pass
+    
+class AWSCloudBuddy():
     def __init__(self):
         # for now, assume environment variables are in
         self.client = boto3.client('ec2')
 
+    def listSubnets(self):
+        self.client.describe_subnets()
+    
+    def listUsers(self):
+        pass
