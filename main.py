13e36b83a53350c128526f3d39dfe24f8e6d5dc6
ck <github@carrollkong.com>
2019-06-13 13:19:58 +0000

    some initial framework

---
+++ main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+#
+#
+import boto3
+
+# get AWS credentials
+# check
+
+
+class CloudBuddy:
+    def __init__(self):
+        pass
+
+
+# client = boto3.client('ec2')
+# use ec2boto client
