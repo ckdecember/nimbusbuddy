0754129b5dd5984ccff158b90eb88de2804b4d8a
ck <github@carrollkong.com>
2019-06-13 17:44:41 +0000

    list subnets works in a rudimentary manner (no filtering yet)

--- main.py
+++ main.py
@@ -13,23 +13,29 @@ class CloudBuddy:
 class AWSCloudBuddy():
     def __init__(self):
         # for now, assume environment variables are in
-        self.client = boto3.client('ec2')
+        self.ec2client = boto3.client('ec2')
+        self.iamclient = boto3.client('iam')
+        # probably not the best data structure for it.
+        self.subnets = None
+        self.ec2instances = None
+        self.iamusers = None
 
     def listSubnets(self):
-        self.client.describe_subnets()
+        return self.ec2client.describe_subnets()
     
     def listInstances(self):
-        self.client.describe_instances()
+        self.ec2client.describe_instances()
 
     def listVPCs(self):
-        pass
+        self.ec2client.describe_vpcs()
 
     def listUsers(self):
-        self.client.list_users()
+        self.iamclient.list_users()
 
 def main():
     acb = AWSCloudBuddy()
     subnet = acb.listSubnets()
+    print (subnet)
 
 if  __name__ =='__main__': main()   
 
