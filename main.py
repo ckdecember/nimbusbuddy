418bf78907b1665ae6fc0c904f79ce5cf516f127
ck <github@carrollkong.com>
2019-06-13 13:44:26 +0000

    more docs for initial setup

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-#!/usr/bin/env python
-#
-#
+"""
+Core control module for cloudbuddy
+
+"""
+
+
 import boto3
 
 # get AWS credentials
-# check
-
+# get from environment variables
+# check for failure on no-auth
+#Boto3 will check these environment variables for credentials:
+#AWS_ACCESS_KEY_ID
+#The access key for your AWS account.
+#AWS_SECRET_ACCESS_KEY
+#The secret key for your AWS account.
+#AWS_SESSION_TOKEN
+#The session key for your AWS account. This is only needed when you are using temporary credentials. The AWS_SECURITY_TOKEN environment variable can also be used, but is only supported for backwards compatibility purposes. AWS_SESSION_TOKEN is supported by multiple AWS SDKs besides python.
 
 class CloudBuddy:
     def __init__(self):
-        pass
+        # for now, assume environment variables are in
+        self.client = boto3.client('ec2')
 
-
-# client = boto3.client('ec2')
-# use ec2boto client
