33a565f7c51417e14b16f4f2681b16480775d074
carroll kong <github@carrollkong.com>
2019-06-23 17:51:00 -0400

    flattened refactoring in AWSVPCs successful.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 """
-Core control module for cloudbuddy
+Core control module for nimbus buddy
 
 """
 
@@ -21,16 +21,14 @@ class NimbusBuddy:
 class AWSNimbusBuddy():
     """ Hey I'm a NimbusBuddy """
     def __init__(self):
-        self.ec2client = boto3.client('ec2', region_name = 'us-east-2')
-        self.ec2clientdict = {}
-        self.dataStruct = {}
-        
-        regionList = self.getAllRegions()
-        
-        self.initRegionList(regionList)
+        # base client for basic get operations
+        self.ec2client = boto3.client('ec2')
 
-        #self.iamclient = boto3.client('iam')
+        # dictionary of ec2 clients for all regions to easily view other regions as needed.
+        regionList = self.getAllRegions()
+        self.ec2clientdict = self.initRegionList(regionList)
         
+        # this will probably be all deprecated
         # we only pull a subset of fields from AWS
         self.vpcKeyList = ['CidrBlock', 'State', 'Tags', 'OwnerId', 'VpcId', 'IsDefault']
         self.vpcExpandedKeyList = ['Tags']
@@ -41,19 +39,18 @@ class AWSNimbusBuddy():
         self.instanceKeyList = ['InstanceId', 'InstanceType', 'ImageId', 'PrivateIpAddress', 'PublicDnsName', 'SubnetId', 'VpcId', 'Placement', 'Tags', 'SecurityGroups', 'OwnerId', 'State']
         self.instanceExpandedKeyList = ['Tags', 'Placement', 'SecurityGroups']
     
-    def getSecurityGroups(self):
-        return self.ec2client.describe_security_groups()['SecurityGroups']
-        
     def getAllRegions(self):
         fullregionlist = self.ec2client.describe_regions()
         regionlist = [i['RegionName'] for i in fullregionlist['Regions']]
         return regionlist
 
     def initRegionList(self, regionList):
+        clientDict = {}
         for region in regionList:
-            self.ec2clientdict[region] = boto3.client('ec2', region_name=region)
-    
-    def cycleRegionInstances(self):
+            clientDict[region] = boto3.client('ec2', region_name=region)
+        return clientDict
+
+    def viewAllRegionInstances(self):
         regionList = self.getAllRegions()
         print ("Start")
         for region in regionList:
@@ -71,6 +68,9 @@ class AWSNimbusBuddy():
                             print ("{} {}".format(instancekey, reservedInstance[instancekey]))
                     print ("#####################")
 
+    def getSecurityGroups(self):
+        return self.ec2client.describe_security_groups()['SecurityGroups']
+        
     def getVPCs(self, regionname):
         return self.ec2clientdict[regionname].describe_vpcs()['Vpcs']
 
@@ -172,6 +172,62 @@ class AWSVPC():
         self.uniqueid = vpcDict['VpcId']
         self.isDefault = vpcDict['IsDefault']
 
+class AWSVPC2():
+    def __init__(self, amazonResourceList):
+        """ flatten some values, keep core dictionary """
+
+        self.resourceList = amazonResourceList
+        self.resourceDictList = []
+        self.keyList = ['CidrBlock', 'State', 'Tags', 'OwnerId', 'VpcId', 'IsDefault']
+        self.expandedKeyList = ['Tags']
+        self.amazonDefaultTagName = 'Name'
+
+        for amazonResource in self.resourceList:
+            resourceDict = {}
+            for (key, sublist) in amazonResource.items():
+                if key in self.expandedKeyList:
+                    # ok these are dicts, or possibly lists of dicts.
+                    # value is a sublist.  need to break it down again.
+                    for subitem in sublist:
+                        # check for missing key
+                        
+                        if subitem['Key'] == 'Name':
+                            resourceDict[key] = subitem['Value']
+                else:
+                    resourceDict[key] = amazonResource[key]
+            self.resourceDictList.append(resourceDict)
+        
+        print ("#######")
+        print ("entering resource Dict")
+        for resourceDict in self.resourceDictList:
+            for (key, value) in resourceDict.items():
+                print (key, value)
+        
+        
+
+
+
+"""
+        for key in self.keyList:
+            if key in originalDict:
+                if key in self.expandedKeyList:
+                    for item in originalDict[key]:
+                        #resourceDict[]
+"""
+
+        #def extractResource(self, slice, keyList, expandedKeyList):
+        #resourceDict = {}
+        #for key in keyList:
+        #    if key in slice:
+        #        # some values are actually lists, expand those lists.
+        #        if key in expandedKeyList:
+        #            for item in slice[key]:
+        #                resourceDict[key] = item['Value']
+        #        else:
+        #            resourceDict[key] = slice[key]
+                # instances are handled differently
+        #return resourceDict
+
 class AWSSubnet():
     def __init__(self, subnetDict):
         #['AvailabilityZone', 'CidrBlock', 'MapPublicIpOnLaunch', 'State', 'SubnetId', 'VpcId', 'OwnerId', 'Tags', 'SubnetArn']
@@ -239,7 +295,7 @@ class AWSSecurityGroups():
     def __init__(self):
         pass
 
-class TestCloudBuddy(unittest.TestCase):
+class TestNimbusBuddy(unittest.TestCase):
     """ Basic Unit Test for boto3 / ec2 instantiation"""
     def test_aws(self):
         client = boto3.client('ec2')
@@ -249,10 +305,18 @@ class TestCloudBuddy(unittest.TestCase):
     #    client = boto3.client('ec2')
     #    isinstance(client.describe_regions(),type(list))
 
+    #def test_aws_region(self):
+    #    regionTest = 'us-east-1'
+    #    client = boto3.client('ec2', region_name=regionTest)
+    #    session = boto3.session.Session()
+    #    region = session.region_name
+    #    self.assertEquals(region, regionTest)
+
+
+
 def outputTerraform(region, targetRegion, ami):
     """ Dumps a terraform configuration as a main.tf and variables.tf file """
     acb = AWSNimbusBuddy()
-    #acb.cycleRegionInstances()
 
     AWSVPCList = acb.getProcessedVPCs(region=region)
     SubnetList = acb.getProcessedSubnets(region=region)
@@ -289,7 +353,7 @@ def howtomerge(region):
 def display(region):
     """ Display Simple Tables of VPCs, Instances, and Subnets """
     print ("display")
-    acb = AWSCloudBuddy()
+    acb = AWSNimbusBuddy()
     # filter dictionaries with dict comprehension
     killlist = ['DhcpOptionsId', 'InstanceTenancy', 'CidrBlockAssociationSet', 'Tags']
     
@@ -325,6 +389,12 @@ def sliceDisplay(dictList, slices, regionname, killlist, allowList=[]):
     return dictList
     #return tabulate.tabulate(dictList, headers='keys')
 
+def testAWSVPC2(region):
+    anb = AWSNimbusBuddy()
+    originalDict = anb.getVPCs(region)
+    vpc2 = AWSVPC2(originalDict)
+
+
 def main():
     parser = argparse.ArgumentParser(description="Cloud Visualization and Backup Tool")
     parser.add_argument('command', help='Action')
@@ -358,6 +428,8 @@ def main():
             print ('need args')
     elif args.command == 'merge':
         howtomerge(args.region)
+    elif args.command == 'test':
+        testAWSVPC2(args.region)
     else:
         noop()
 
